52d2c49c6827fe8db00389b4a39d93d606b5e225
Tyjoo26 <Tyjoo21@gmail.com>
2018-02-07 11:03:43 -0700

    Stuck on search controller response

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,8 +1,12 @@
 class SearchController < ApplicationController
 
   def index
-    @conn = Faraday.new(:url => 'http://api.data.gov') do |faraday|
-      faraday.headers["api_key"] = "ENV[API-KEY]"
+    @conn = Faraday.new(:url => 'https://developer.nrel.gov') do |faraday|
+      faraday.headers["X-API-KEY"] = "ENV[API_KEY]"
     end
+
+
+    station_info = @conn.get "/api/alt-fuel-stations/v1/nearest.json?location=#{params["q"]}&fuel_type=ELEC"
+    binding.pry
   end
 end
