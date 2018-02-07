b981756dd673805e249ec97afc5e5b03da3e2833
Tyjoo26 <Tyjoo21@gmail.com>
2018-02-07 11:07:46 -0700

    figured out get for station infO'

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,12 +1,10 @@
 class SearchController < ApplicationController
 
   def index
-    @conn = Faraday.new(:url => 'https://developer.nrel.gov') do |faraday|
-      faraday.headers["X-API-KEY"] = "ENV[API_KEY]"
-    end
+    @conn = Faraday.new(:url => 'https://developer.nrel.gov')
 
 
-    station_info = @conn.get "/api/alt-fuel-stations/v1/nearest.json?location=#{params["q"]}&fuel_type=ELEC"
+    station_info = @conn.get "/api/alt-fuel-stations/v1/nearest.json?api_key=#{ENV["api_key"]}&location=80203&"
     binding.pry
   end
 end
