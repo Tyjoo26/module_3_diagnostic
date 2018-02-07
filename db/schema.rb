8c3da5388fef51b91247a12c41f543a37ebf3aa3
Tyjoo26 <Tyjoo21@gmail.com>
2018-02-07 09:32:35 -0700

    modfiy schema for station migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20180207163124) do
+
+  create_table "stations", force: :cascade do |t|
+    t.string "name"
+    t.string "address"
+    t.string "fuel_type"
+    t.decimal "distance"
+    t.string "access_times"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
