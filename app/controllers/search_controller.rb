889c55a7aba111ef042ec17e999c87e8d16e96fd
Tyjoo26 <Tyjoo21@gmail.com>
2018-02-07 11:10:30 -0700

    final commit

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -4,7 +4,11 @@ class SearchController < ApplicationController
     @conn = Faraday.new(:url => 'https://developer.nrel.gov')
 
 
-    station_info = @conn.get "/api/alt-fuel-stations/v1/nearest.json?api_key=#{ENV["api_key"]}&location=80203&"
+    station_info = @conn.get "/api/alt-fuel-stations/v1/nearest.json?api_key=#{ENV["api_key"]}&location=80203&fuel_type=ELEC&fuel_type=LPG"
+
+    station_response = JSON.parse(station_info.body, symbolize_names: true)
+
+    redirect_to search_path
     binding.pry
   end
 end
