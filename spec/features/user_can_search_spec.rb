19f582b49869f6d3aa8d985156971ddf09fab9f8
Tyjoo26 <Tyjoo21@gmail.com>
2018-02-07 09:19:52 -0700

    initial commit, building out basic feature test, working through figuring out how i want to build my test

--- spec/features/user_can_search_spec.rb
+++ spec/features/user_can_search_spec.rb
@@ -1,11 +1,13 @@
 
-
 feature "User can visit root page" do
-  scenario "and view navbar contents" do
+  scenario "and search by zipcode" do
     visit "/"
     within(".navbar") do
       expect(page).to have_content("AltFuelFinder")
       expect(page).to have_selector("input[value='Search by zip...']")
     end
+
+    expect(current_path).to eq(search_stations_path)
+    expect(page).to have_content("Station")
   end
 end
